b8aa41f2dbd758eb34ca74cb6fbf33c25cfec6d0
loloTheMagician <elperiplodelolo@gmail.com>
2024-12-06 11:44:31 +0100

    refactor: old relative paths lenis + gsap

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-import gsap from '../node_modules/gsap/index.js';
-import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
-import Lenis from "../node_modules/lenis/dist/lenis.mjs";
+import Lenis from "leins";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger)
 // Initialize a new Lenis instance for smooth scrolling
